ee3ae35480e0aa57b641113928998e68b801f136
Alex <40923759+rednexela1941@users.noreply.github.com>
2021-01-19 16:09:20 -0500

    fix do

--- client.go
+++ client.go
@@ -1,8 +1,9 @@
 package canopen
 
 import (
-	"github.com/brutella/can"
 	"time"
+
+	"github.com/brutella/can"
 )
 
 // A Client handles message communication by sending a request
@@ -15,11 +16,13 @@ type Client struct {
 // Do sends a request and waits for a response.
 // If the response frame doesn't arrive on time, an error is returned.
 func (c *Client) Do(req *Request) (*Response, error) {
+	rch := can.Wait(c.Bus, req.ResponseID, c.Timeout)
+
 	if err := c.Bus.Publish(req.Frame.CANFrame()); err != nil {
 		return nil, err
 	}
 
-	resp := <-can.Wait(c.Bus, req.ResponseID, c.Timeout)
+	resp := <-rch
 
 	return &Response{CANopenFrame(resp.Frame), req}, resp.Err
 }
