8f397a2ca5e76c74f3e11cbe7b84191225046776
Fabian Petersen <fabian.petersen@etteplan.com>
2021-11-05 14:31:41 +0100

    Prevent multiple request to be made to the same device before a response is returned

--- client.go
+++ client.go
@@ -1,10 +1,13 @@
 package canopen
 
 import (
-	"time"
-
 	"github.com/FabianPetersen/can"
+	"github.com/jpillora/maplock"
+	"strconv"
+	"time"
 )
+
+var m = maplock.New()
 
 // A Client handles message communication by sending a request
 // and waiting for the response.
@@ -16,6 +19,11 @@ type Client struct {
 // Do sends a request and waits for a response.
 // If the response frame doesn't arrive on time, an error is returned.
 func (c *Client) Do(req *Request) (*Response, error) {
+	// Do not allow multiple messages for the same device
+	key := strconv.Itoa(int(req.ResponseID))
+	m.Lock(key)
+	defer m.Unlock(key)
+
 	rch := can.Wait(c.Bus, req.ResponseID, c.Timeout)
 
 	if err := c.Bus.Publish(req.Frame.CANFrame()); err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/FabianPetersen/canopen
 
 go 1.15
 
-require github.com/FabianPetersen/can v0.0.3
+require (
+	github.com/FabianPetersen/can v0.0.3
+	github.com/jpillora/maplock v0.0.0-20160420012925-5c725ac6e22a
+)
